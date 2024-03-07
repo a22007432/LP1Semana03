1cc7f16606157f66f47ada9efb1752270dc47895
a22007432 <ssm.antonio@gmail.com>
2024-03-07 19:12:00 +0000

    done ex 5

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -6,7 +6,18 @@ namespace PowTwoUntil
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for(int j = 1; j<2; j++)
+            {
+                PowersOf2Until5();
+            }
+        }
+
+        private static void PowersOf2Until5()
+        {
+            for(int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
