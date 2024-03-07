b2339bf1a22209ab54271e1c7b1f44ebb66d562b
a22007432 <ssm.antonio@gmail.com>
2024-03-07 14:47:18 +0000

    ex 1 done

---
+++ RightSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string s in args)
+            {
+                if(s.Length > 3)
+                {
+                    
+                    
+                    if(s.Length > 6)
+                    {
+                        return;
+                    }
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
