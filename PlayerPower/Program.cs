6042a41c8e50601d833c4ad1575b7462df43dbe5
a22007432 <ssm.antonio@gmail.com>
2024-03-09 13:12:39 +0000

    tweaked

--- PlayerPower/Program.cs
+++ PlayerPower/Program.cs
@@ -9,9 +9,9 @@ namespace PlayerPower
 
             
 
-            UserPowers userPower = 0;
-
-            int[] playerArray;
+            
+            //int[] playerArray;
+            string ret = "";
             
             
 
@@ -19,74 +19,65 @@ namespace PlayerPower
             int n = int.Parse(Console.ReadLine());
 
 
-            playerArray = new int[n];
+            UserPowers [] playerArray = new UserPowers[n];
 
 
 
             for(int i = 0; i < n; i++)
             {
-                Console.WriteLine($"Please state your powers, player {i+1}:");
+                ret += $"Player{i+1}";
+                Console.WriteLine($"State your powers, player {i+1}:");
                 Console.WriteLine("1 => Flight");
                 Console.WriteLine("2 => X-ray Vision");
                 Console.WriteLine("3 => Super Strength");
                 Console.WriteLine("0 => Nothing");
                 Console.WriteLine("anything above 3 => Nothing");
-                Console.Write(">>");
-
-                //use input sting as array to get the chars
-                string tmpChars = Console.ReadLine();
-                //this doesnt get the chars it gets the binary code of it
-
+                
 
                 //verify the chars
-                foreach(char tmpc in tmpChars)
+                for (int j = 0; j <3; j++)
                 {
-                    //Console.WriteLine($"this is the char {tmpc}");
-                    switch(tmpc)
+                    Console.Write(">>");
+
+                    string tmps = Console.ReadLine();
+
+
+                    switch(tmps)
                     {
-                        case '1':
-                            userPower += (int) UserPowers.Fly;
+                        case "1":
+                            playerArray[i] ^=  UserPowers.Fly;
                             break;
-                        case '2':
-                            userPower += (int) UserPowers.XRayVision;
+                        case "":
+                            playerArray[i] ^=  UserPowers.XRayVision;
                             break;
-                        case '3':
-                            userPower += (int) UserPowers.SuperStrength;
+                        case "3":
+                            playerArray[i] ^= UserPowers.SuperStrength;
                             break;
                         default:
                             break;
                         
                     }
-                    playerArray[i] += (int) userPower;
 
-                    userPower = 0;
+                    ret += $" {playerArray[i]}\n";
 
+                    if ((playerArray[i] & UserPowers.Fly) == UserPowers.Fly
+                    && ((playerArray[i] & UserPowers.SuperStrength)
+                     == UserPowers.SuperStrength))
+                {
+                    ret +=" Flying radiation!\n";
                 }
+   
+                }
+                Console.WriteLine(ret);
+                
 
-                /*playerArray[i] += (int) userPower;
 
-                userPower = 0;*/
-
+            
                 
             }
             
+            
 
-            for(int j = 0; j < n; j++)
-            {
-                Console.WriteLine($"Player {j + 1}: {playerArray[j]}");
-                userPower += playerArray[j];
-                //userPower += playerArray[j];
-                Console.WriteLine(userPower);
-                Console.WriteLine("");
-
-                UserPowers retpowers = (UserPowers) playerArray[j];
-                Console.WriteLine($"this is ret power {retpowers}");
-                //if(playerArray[j])
-              
-
-
-
-            }
 
 
 
